10ffdb3fef42c75d93cfc1bd0a69d3c3d1b9b68e
AhmedRamiBoukef <ahmedramiboukef@gmail.com>
2022-11-01 00:05:56 +0100

    Adding Expertises section

--- components/Expertise/Expertise.jsx
+++ components/Expertise/Expertise.jsx
@@ -1,33 +1,46 @@
 import React from 'react'
+import Image from "next/image";
 import { Title } from '../Shared/Title' 
-import {react} from "../../images/icons/react.svg"
-import {flutter} from "../../images/icons/flutter.svg"
-import {node} from "../../images/icons/nodejs.svg"
+import react from "../../images/icons/frontend.svg"
+import flutter from "../../images/icons/mobile.svg"
+import node from "../../images/icons/backend.png"
 
 export const Expertise = () => {
   const expertises = [
     {
       title: "Backend Dev NodeJS, Express, MongoDB",
       info: "Over 2 years of development experience in building API'S with NodeJS, ExpressJS frameworks adn MongoDB for the Database.",
-      logo: node
+      logo: node,
+      style: "w-full md:w-1/2 lg:w-1/3 flex flex-col gap-6 py-10 px-8 border-2 border-black"
     },
     {
       title: "Frontend Dev React, NextJS",
       info: "Passionate about UI/UX. Over 2 years of development experience in HTML, CSS, JS, React and NextJS frameworks.",
-      logo: react
+      logo: react,
+      style: "w-full md:w-1/2 lg:w-1/3 flex flex-col gap-6 py-10 px-8 md:border-l-0 border-t-0 md:border-t-2 border-2 lg:border-r-0 border-black"
     },
     {
       title: "Flutter Dev Android, iOS",
       info: "Skilled in developing hybrid mobile apps and cross-platform solutions using the Flutter framework.",
-      logo: flutter
+      logo: flutter,
+      style: "w-full md:w-1/2 lg:w-1/3 flex flex-col gap-6 py-10 px-8 border-2 border-t-0 lg:border-t-2 border-black"
     }
   ]
   return (
-    <div className='h-[100vh] px-12 flex flex-col justify-center items-center' id='Expertise'>
+    <div className='my-6 px-12 flex flex-col gap-8 items-center' id='Expertise'>
       <Title className="mx-auto mb-14"  title="My Expertise"/>
-      <div>
+      <div className='flex flex-wrap'>
         {expertises.map(elem => (
-          <div key={elem.title}>
+          <div key={elem.title} className={elem.style}>
+            <div className='flex gap-6'>
+              <div>
+                <Image src={elem.logo} alt={elem.title} height="80px" width="80px" />
+              </div>
+              <h1 className='text-2xl font-semibold leading-5 my-1.5'>{elem.title}</h1>
+            </div>
+            <p className='leading-6 px-10'>
+              {elem.info}
+            </p>
 
           </div>
         ))}
--- components/Shared/SkillItem.jsx
+++ components/Shared/SkillItem.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 export const SkillItem = ({title,imageURL}) => {
   return (
     <div className='p-2 aspect-square w-1/2 md:w-3/12 xl:w-2/12'>
-        <div className='bg-yellow-200 rounded'>
+        <div className='bg-[#00509D] rounded shadow-2xl '>
             <div className='p-6'>
                 <div className='relative h-[80px] flex justify-center items-center w-auto max-w-full pointer-events-none'>
-                    <Image src={imageURL} alt={title} height={title === "Next.js" ? "":"80px"}/>
+                    <Image src={imageURL} alt={title} height={title === "Next.js" ? "":"80px"} width={title === "Next.js" ? "":"80px"}/>
                 </div>
             </div>
-            <div className='w-full bg-yellow-300 px-4 py-2 min-h-[43.5px] rounded-b'>
+            <div className='w-full bg-[#11296B] px-4 py-2 min-h-[43.5px] rounded-b'>
                 <span class="block text-white text-center font-mono leading-snug text-xl">{title}</span>
             </div>
         </div>
